f1da87f67693b369f4af4fa908eb47395783b17f
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-04 19:39:48 +0530

    Included Stack

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import Stack_Module
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -12,5 +12,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    a1=Stack_Module.Stack():
+    a1.push(3)
+    a1.pop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
