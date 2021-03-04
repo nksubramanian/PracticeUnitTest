e86c19a4a6e1eb34ebbd9427991410f40bf12968
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-04 19:43:23 +0530

    Included Stack

--- venv/Test_StackModule.py
+++ venv/Test_StackModule.py
@@ -4,3 +4,9 @@ def test_Stack():
     s1.push(1)
     temp=s1.pop()
     assert temp==1
+    s1.push(1)
+    s1.push(2)
+    s1.push(3)
+    assert s1.pop()==3
+    assert s1.pop() == 2
+    assert s1.pop() == 1
